e755a1473ca897dd8a6bb77534de7488d708be18
Jez Barnsley <jbarnsley.github@gmail.com>
2026-09-23 08:22:04 +0100

    Lint fix

--- src/server/oidc/provider-config.js
+++ src/server/oidc/provider-config.js
@@ -37,9 +37,7 @@ const COOKIE_LONG = /** @type {CookiesSetOptions} */ ({
  */
 const RESOURCE_SERVERS = new Set(config.get('oidc.resourceServers'))
 
-const REFRESH_TOKEN_TTL = /** @type number */ config.get(
-  'oidc.ttl.refreshToken'
-)
+const REFRESH_TOKEN_TTL = config.get('oidc.ttl.refreshToken')
 
 const TTL_SECONDS = {
   AuthorizationCode: config.get('oidc.ttl.authorizationCode'),
